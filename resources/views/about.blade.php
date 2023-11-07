3d78724779985a1180206df91d754dd4fb312b3a
Daniil <D2012L@mail.ru>
2023-11-07 08:34:22 +0300

    laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title')О нас@endsection
+
+@section('content')
+    <h1>О нас</h1>
+@endsection
+
+@section('aside')
+    @parent
+    <p>1111</p>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title')Контакт@endsection
+
+@section('content')
+    <h1>Контакт</h1>
+@endsection
