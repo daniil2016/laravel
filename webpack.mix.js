26a642d2afa7e876611f1e45aa1dccba07fbb162
Daniil <D2012L@mail.ru>
2023-11-08 07:12:20 +0300

    123123

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,2 +1,8 @@
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css');
+//mix.js('resources/js/app.js', 'public/js')
+    //.postCss('resources/css/app.css', 'public/css');
+
+    const { mix } = require('laravel-mix');
+    const path = require('path');
+    const glob = require('glob');
+    
+    mix.js(glob.sync('resources/js/**/*.js'), 'public/js/app.js');
